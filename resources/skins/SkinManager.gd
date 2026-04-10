61b8f8f529bec67fb449f981f020d4afcfa6f050
MarcMeRu11 <marcmeru@gmail.com>
2026-04-10 16:30:53 +0200

    Highscore download fix

--- resources/skins/SkinManager.gd
+++ resources/skins/SkinManager.gd
@@ -2,9 +2,11 @@ extends Node
 
 var all_skins: Array[SkinData] = []
 var selected_skin: SkinData
+var player_highscore: float = 0.0
 
 func _ready() -> void:
 	load_skins_manually()
+	fetch_highscore_from_talo()
 
 func load_skins_manually():
 	var path = "res://resources/skins/"
@@ -17,37 +19,27 @@ func load_skins_manually():
 			var skin = load(full_path)
 			if skin is SkinData:
 				all_skins.append(skin)
-	
-	print("Skins cargadas en Manager: ", all_skins.size())
 
-# Función para guardar
+func fetch_highscore_from_talo():
+	if Talo.current_player:
+		var options := Talo.leaderboards.GetEntriesOptions.new()
+		options.player_id = Talo.current_player.id
+		
+		var res = await Talo.leaderboards.get_entries("main", options)
+		
+		if res.entries.size() > 0:
+			player_highscore = res.entries[0].score
+			print("Record de Talo recuperado: ", player_highscore)
+		else:
+			player_highscore = 0.0
+
+func is_skin_unlocked(skin: SkinData) -> bool:
+	if skin.required_score <= 0:
+		return true
+	
+	return player_highscore >= skin.required_score
+
 func save_skin_to_talo(skin_to_save: SkinData):
 	if Talo.current_player:
 		Talo.current_player.set_prop("selected_skin", skin_to_save.skin_name)
 		selected_skin = skin_to_save
-
-# Función para cargar al inicio
-func load_skin_from_talo() -> SkinData:
-	if Talo.current_player:
-		var skin_name = Talo.current_player.get_prop("selected_skin")
-		if skin_name:
-			for s in all_skins:
-				if s.skin_name == skin_name:
-					selected_skin = s
-					return s
-	
-	if all_skins.size() > 0:
-		selected_skin = all_skins[0]
-	return selected_skin
-
-# Función de desbloqueo (ahora más simple y robusta)
-func is_skin_unlocked(skin: SkinData) -> bool:
-	if skin.required_score <= 0:
-		return true
-		
-	if Talo.current_player:
-		var prop_value = Talo.current_player.get_prop("highscore")
-		if prop_value != null:
-			return float(prop_value) >= skin.required_score
-			
-	return false
